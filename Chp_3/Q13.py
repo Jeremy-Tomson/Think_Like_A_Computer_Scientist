a2b68c0c8015556a57362df4f7c06434a7dba99e
Jeremy-Tomson <48037011+Jeremy-Tomson@users.noreply.github.com>
2019-11-07 09:29:51 +0800

    Add files via upload

---
+++ Chp_3/Q13.py
@@ -0,0 +1,8 @@
+# Q13
+
+import turtle
+alex = turtle.Turtle()
+b = alex
+print(type(b))
+
+# Displayed class is 'turtle.Turtle'
---
+++ Chp_3/Q2.py
@@ -0,0 +1,12 @@
+# Q2
+
+import turtle
+cellphone = turtle.Turtle()
+# 3 Attributes
+cellphone.turtle.colour("Green")
+cellphone.speed(100)
+cellphone.turtle.size(50)
+# 3 Methods
+cellphone.forward(100)
+cellphone.backward(100)
+cellphone.stamp()
