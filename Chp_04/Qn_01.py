3c2fa4d69b788bf13e7ba07544f731e4f56ea09b
Jeremy-Tomson <48037011+Jeremy-Tomson@users.noreply.github.com>
2019-12-02 13:16:15 +0800

    Add files via upload

---
+++ Chp_04/Qn_01.py
@@ -0,0 +1,25 @@
+# Q1
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+jeremy = turtle.Turtle()
+jeremy.color("hotpink")
+jeremy.pensize(3)
+
+
+def draw_square(t, sz):
+    for i in range(4):
+        t.forward(sz)
+        t.left(90)
+    t.penup()
+    t.forward(sz * 2)
+    t.pendown()
+
+
+draw_square(jeremy, 30)
+draw_square(jeremy, 30)
+draw_square(jeremy, 30)
+draw_square(jeremy, 30)
+draw_square(jeremy, 30)
+
+wn.mainloop()
---
+++ Chp_04/Qn_04.py
@@ -0,0 +1,19 @@
+# Q4
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+jeremy = turtle.Turtle()
+jeremy.color("blue")
+jeremy.pensize(1)
+
+
+def draw_square(t, sz):
+    for i in range(4):
+        t.forward(sz)
+        t.left(90)
+
+
+for i in range(24):
+    draw_square(jeremy, 100)
+    jeremy.right(15)
+wn.mainloop()
