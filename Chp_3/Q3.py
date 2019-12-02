11ecbc58c45cfea6af6edbfc8a78b0765aa808a8
Jeremy-Tomson <48037011+Jeremy-Tomson@users.noreply.github.com>
2019-12-02 12:41:15 +0800

    Delete Q3.py

--- Chp_3/Q3.py
+++
@@ -1,4 +0,0 @@
-# Q3
-
-for i in ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]:
-    print("One of the months of the year is", i)
