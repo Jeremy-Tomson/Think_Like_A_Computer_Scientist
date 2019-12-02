d660f93a346af583f703a59b6b34567e8d056d93
Jeremy-Tomson <48037011+Jeremy-Tomson@users.noreply.github.com>
2019-12-02 12:39:42 +0800

    Delete Q8.py

--- Chp_4/Q8.py
+++
@@ -1,9 +0,0 @@
-# Q8
-def area_of_circle(r, p):
-    a = (r**2) * p
-    return a
-
-
-radius = float(input("What is the radius of the circle?"))
-area = area_of_circle(radius, 3.14)
-print("The circle with radius,", radius, "has an area of,", area, "units.")
